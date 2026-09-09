62a32255a3fa3ac2733c583c7dc09a05d7a2fc45
Dan Gilleland <dgilleland@nait.ca>
2026-09-09 11:42:10 -0600

    Astronaut demo

--- src/A/review-astronauts-example-START/js/dom/astronaut.js
+++ src/A/review-astronauts-example-START/js/dom/astronaut.js
@@ -12,3 +12,25 @@ replace the instances that have "THIS FORMAT HERE" with the astronautData.
     <p class="mb-1">ASTRONAUT BIO HERE </p>
 </li>
 */
+
+/**
+ * 
+ * @param {{name:string, bio:string, agency:string, status:string, date_of_birth:string, in_space:string, nationality:string, profile_image_thumbnail:string}} astronautData Information about a single astronaut
+ * @param {HTMLUListElement} listElement 
+ */
+const renderAstronautListItem = function(astronautData, listElement) {
+    // TODO: complete this function stub
+    const { name, bio, agency, status, date_of_birth, in_space, nationality, profile_image_thumbnail } = astronautData; // using Object Destructuring
+    const listItem = `<li class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+            <img src="${profile_image_thumbnail}" class="rounded float-start" alt="${name} thumbnail" />
+            <h5 class="mb-1">${name}</h5>
+            <small class="float-end">born ${date_of_birth}</small>
+        </div>
+        <small>${nationality} (${agency.name})</small>
+        <p class="mb-1">${bio}</p>
+    </li>`
+    listElement.innerHTML += listItem;
+}
+
+export { renderAstronautListItem }
--- src/A/review-astronauts-example-START/js/index.js
+++ src/A/review-astronauts-example-START/js/index.js
@@ -1,1 +1,14 @@
 // js here.
+import 'bootstrap/dist/css/bootstrap.min.css';
+//     ^ look in the node_modules folder
+import { getAstronautList } from './api/astronaut';
+import { renderAstronautListItem } from './dom/astronaut';
+
+getAstronautList().then(data => {
+    console.log(data);
+    // use the data.results array and render each astronaut
+    const listContainer = document.querySelector('#astronaut-list');
+    data.results.forEach(person => {
+        renderAstronautListItem(person, listContainer);
+    });
+});
